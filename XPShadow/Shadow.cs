01b47fd28c0f6eefd3d6ae91b728723d0e388428
brookshi <iwxiaot@gmail.com>
2015-10-17 18:08:35 +0800

    add project

---
+++ XPShadow/Shadow.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Graphics.Effects;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+
+namespace XP
+{
+    public sealed class Shadow : ContentControl
+    {
+        const int MAX_Z_DEPTH = 5;
+        const int MIN_Z_DEPTH = 1;
+
+        CanvasControl _shadowCanvas;
+        ContentPresenter _contentPresenter;
+        Grid _topGrid;
+
+        double _shadowWidth = 0;
+        double _shadowHeight = 0;
+
+        public int Z_Depth
+        {
+            get { return (int)GetValue(Z_DepthProperty); }
+            set { SetValue(Z_DepthProperty, value); }
+        }
+        public static readonly DependencyProperty Z_DepthProperty =
+            DependencyProperty.Register("Z_Depth", typeof(int), typeof(Shadow), new PropertyMetadata(2));
+
+        public Shadow()
+        {
+            this.DefaultStyleKey = typeof(Shadow);
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _shadowCanvas.Draw += OnDraw;
+        }
+
+        private void OnDraw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            if (Z_Depth < MIN_Z_DEPTH)
+                return;
+
+            var shadowParams = ShadowConfig.GetShadowParamForZDepth(Math.Min(Z_Depth, MAX_Z_DEPTH));
+            DrawShadow(sender, args.DrawingSession, shadowParams);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _shadowCanvas = (CanvasControl)GetTemplateChild("ShadowCanvas");
+            _contentPresenter = (ContentPresenter)GetTemplateChild("PART_ContentPresenter");
+            _topGrid = (Grid)GetTemplateChild("PART_Grid");
+        }
+
+        void DrawShadow(CanvasControl canvasCtrl, CanvasDrawingSession drawSession, List<ShadowParam> shadowParams)
+        {
+            var canvasCommandList = new CanvasCommandList(canvasCtrl);
+            var content = _contentPresenter.Content as FrameworkElement;
+            var contentWidth = content.ActualWidth + content.Margin.Left + content.Margin.Right;
+            var contentHeight = content.ActualHeight + content.Margin.Top + content.Margin.Bottom;
+
+            _shadowCanvas.VerticalAlignment = content.VerticalAlignment;
+            _shadowCanvas.HorizontalAlignment = content.HorizontalAlignment;
+
+            using (var ds = canvasCommandList.CreateDrawingSession())
+            {
+                ds.FillRoundedRectangle(new Rect(0, 0, contentWidth, contentHeight), 0, 0, Color.FromArgb(255, 0, 0, 0));
+            }
+
+            CompositeEffect compositeEffect = new CompositeEffect();
+            double maxOffset_Y = 0;
+
+            shadowParams.ForEach(param => 
+            {
+                maxOffset_Y = param.Offset_Y;
+                var shadowEffect = CreateShadowEffect(canvasCommandList, param);
+                compositeEffect.Sources.Add(shadowEffect);
+            });
+
+            var bound = compositeEffect.GetBounds(drawSession);
+            _shadowWidth = (bound.Width - contentWidth) / 2;
+            _shadowHeight = (bound.Height - contentHeight) / 2;
+            _contentPresenter.Margin = new Thickness(_shadowWidth, _shadowHeight - maxOffset_Y, _shadowWidth, _shadowHeight + maxOffset_Y);
+            _shadowCanvas.Height = bound.Height;
+            _shadowCanvas.Width = bound.Width;
+            
+            drawSession.DrawImage(compositeEffect, (float)_shadowWidth, (float)_shadowHeight);
+        }
+
+        Transform2DEffect CreateShadowEffect(IGraphicsEffectSource source, ShadowParam param)
+        {
+            return new Transform2DEffect
+            {
+                Source = new Transform2DEffect
+                {
+                    Source = new ShadowEffect
+                    {
+                        BlurAmount = param.Blur,
+                        ShadowColor = Color.FromArgb(param.Alpha, 0, 0, 0),
+                        Source = source
+                    },
+                },
+            };
+        }
+    }
+}
---
+++ XPShadow/ShadowConfig.cs
@@ -0,0 +1,53 @@
+﻿#region License
+//   Copyright 2015 Brook Shi
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License. 
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XP
+{
+    public class ShadowParam
+    {
+        public byte Alpha { get; set; }
+
+        public float Blur { get; set; }
+
+        public int Offset_Y { get; set; }
+    }
+
+    public static class ShadowConfig
+    {
+        private static Dictionary<int, List<ShadowParam>> _shadowParamsWithZDepth = new Dictionary<int, List<ShadowParam>>()
+        {
+            [1] = new List<ShadowParam>() { new ShadowParam() { Alpha = 61, Blur = 1.5f, Offset_Y = 1 }, new ShadowParam() { Alpha = 31, Blur = 2f, Offset_Y = 1 } },
+            [2] = new List<ShadowParam>() { new ShadowParam() { Alpha = 59, Blur = 3f, Offset_Y = 3 }, new ShadowParam() { Alpha = 41, Blur = 3f, Offset_Y = 3 } },
+            [3] = new List<ShadowParam>() { new ShadowParam() { Alpha = 59, Blur = 4f, Offset_Y = 3 }, new ShadowParam() { Alpha = 48, Blur = 5f, Offset_Y = 5 } },
+            [4] = new List<ShadowParam>() { new ShadowParam() { Alpha = 56, Blur = 5f, Offset_Y = 4 }, new ShadowParam() { Alpha = 64, Blur = 9f, Offset_Y = 7 } },
+            [5] = new List<ShadowParam>() { new ShadowParam() { Alpha = 56, Blur = 7f, Offset_Y = 5 }, new ShadowParam() { Alpha = 77, Blur = 12f, Offset_Y = 10 } },
+        };
+
+        public static List<ShadowParam> GetShadowParamForZDepth(int z_depth)
+        {
+            if (z_depth < 1 || z_depth > 5)
+                throw new ArgumentException("z_depth should between 1 to 5");
+
+            return _shadowParamsWithZDepth[z_depth];
+        }
+    }
+}
