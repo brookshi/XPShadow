fa326f1462e09a754d1ffa8de4ae4171b494a3c7
brookshi <iwxiaot@gmail.com>
2015-10-28 18:20:46 +0800

    debug shadow

--- XPShadow/Properties/AssemblyInfo.cs
+++ XPShadow/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("XPShadow")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Shadow for uwp control base on win2d")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("BrookShi")]
 [assembly: AssemblyProduct("XPShadow")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © BrookShi 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.6.0")]
+[assembly: AssemblyFileVersion("0.0.6.0")]
 [assembly: ComVisible(false)]
